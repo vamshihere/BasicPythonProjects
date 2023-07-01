abff37e31f6c441968e73010bf82cf3abef12dc2
vamshihere <samsettyvamshi35@gmail.com>
2023-07-01 17:15:21 +0530

    Madlib code

---
+++ MadLibs.py
@@ -0,0 +1,6 @@
+# We have learnt what is a f string and how to tell the computer that youe are just entering into a new line in editor
+var1 = input("give a synonym of play")
+learn = input("give a synonym of learn")
+parah = f"Hello there dont get fooled by the name madlib it just another {var1} and you are \
+you will basically {learn} two things like me, Adios Amigo! "
+print(parah)
