f6739b0baf1e920608f3a6f1debeaf752acc50f6
vamshihere <samsettyvamshi35@gmail.com>
2023-07-06 12:03:30 +0530

    4 th project

---
+++ rock_paper_scissor.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+    user = input(" enter 'r' for rock, 'p' for paper and 's' for scissor : ")
+    comp = random.choice(['r','p','s'])
+    if(win(user,comp) == 1):
+        return "you won"
+    elif(win(user,comp) == 0):
+        return "it's a tie"
+    else:
+        return "you lost"
+
+def win(user,comp):
+    if(user == comp):
+        return 0
+    if((user == 'r' and comp == 's') or (user == 'p' and comp == 'r') or (user == 's' and comp == 'p')):
+        return 1
+    else:
+        return -1
+
+print(play())    
