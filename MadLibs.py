5d8f5ccdb32f1284065faef27da04fb43484d862
vamshihere <samsettyvamshi35@gmail.com>
2023-09-30 10:51:37 +0530

    refactor

--- MadLibs.py
+++ MadLibs.py
@@ -1,6 +1,6 @@
 # We have learnt what is a f string and how to tell the computer that youe are just entering into a new line in editor
-var1 = input("give a synonym of play")
-learn = input("give a synonym of learn")
-parah = f"Hello there dont get fooled by the name madlib it just another {var1} and you are \
+var1 = input("give a synonym of play : ")
+learn = input("give a synonym of learn : ")
+parah = f"Hello there dont get fooled by the name madlib it just another game where you {var1} and \
 you will basically {learn} two things. Adios Amigo! "
 print(parah)
